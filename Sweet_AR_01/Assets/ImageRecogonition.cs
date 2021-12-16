cf99cd9cf3265b3314481101a652aad1a8023e77
110719008 <48972788+1107719008@users.noreply.github.com>
2021-12-16 15:09:57 +0800

    set the charactor
    image recogonize done

---
+++ Sweet_AR_01/Assets/ImageRecogonition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecogonition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+    }
+
+    private void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+
+    private void OnDisable()
+    {
+
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+        
+    }
+
+}
